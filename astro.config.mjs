196aae112d5d21afbd38e66f67cd1697fea08fc5
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2023-08-24 22:25:38 +0100

    feat: :sparkles: Add sitemap and robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   adapter: vercel(),
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
+  site: 'https://smatysiak.dev',
 });
